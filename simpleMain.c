54b2b54b35664def2509da3cdb904bdff361d2a0
ForestGoose <chayawat.pahn1910@gmail.com>
2021-01-29 09:17:41 +0000

    Add selection.h & try that.

--- simpleMain.c
+++ simpleMain.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "bubble.h"
+#include "selection.h"
 
 void main(int argc, char **argv){
   printf("There are %d arguments\n",argc-1);
@@ -8,6 +10,10 @@ void main(int argc, char **argv){
   //printf("argument 0 is %s\n",*argv);
   for(i=1;i<argc;i++)
     a[i-1] = atoi(argv[i]);
+  /*
   for(i=0;i<N;i++) printf("%d ",a[i]);
   printf("\n");
+  */
+  //bubble(a,N);
+  //selection(a,N);
 }
